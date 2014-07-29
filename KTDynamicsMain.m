bb9fcba25a986936e9ef3b1ab0a2a3f2d7bd4448
mporter-gre <m.porter@dundee.ac.uk>
2014-07-29 15:53:36 +0100

    Plan

--- KTDynamicsMain.m
+++ KTDynamicsMain.m
@@ -10,3 +10,22 @@ spotFeaturesForAnalysisCenpA = getLongTracks(spotFeaturesCenpA, 10);
 spotFeaturesHec1 = getSpotFeatures(modelHec1);
 spotFeaturesForAnalysisHec1 = getLongTracks(spotFeaturesHec1, 10);
 
+%Define the metaphase plate for each time point
+
+
+
+%Measure the distance from each point to the plate
+
+
+
+%Pair up CenpA and Hec1 spots per time point
+
+
+
+
+%Measure distance between pairs of CenpA and Hec1 spots
+
+
+
+
+%Identify sisters and measure the distances per time point
